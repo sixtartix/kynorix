dc21462c2dc3c69123490c0119d15744ab74e767
sixtar_tix <131307954+sixtartix@users.noreply.github.com>
2024-08-20 00:03:52 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+window.addEventListener('scroll', function() {
+    const header = document.querySelector('header');
+    header.classList.toggle('scrolled', window.scrollY > 0);
+});
